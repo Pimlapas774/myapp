fdd60be688c99d58ba4652b94a52b0fb4f540f77
mild74 <pimlapas774@gmail.com>
2021-07-17 01:07:02 +0700

    add constructor and getter to Animal.java

--- src/myapp/Animal.java
+++ src/myapp/Animal.java
@@ -2,4 +2,13 @@ package myapp;
 
 public class Animal {
     private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
